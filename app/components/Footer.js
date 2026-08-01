1b4a22f3e665ce456bc97d0601895d1fa5a78f07
erik696969-a11y <erik.696969@gmail.com>
2026-08-02 01:31:24 +0200

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="text-center text-xs text-harbor/50 py-6 px-4">
       <p className="font-semibold">Hacienda del Señorío de Cifuentes</p>
-      <p>Calle Torre Campanilla 7, Benahavís 29679, España</p>
+      <p>Calle Torre Campanillas 7, Benahavís 29679, España</p>
     </footer>
   );
 }
