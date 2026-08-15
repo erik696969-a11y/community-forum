4591280f07b27dff6acfe943bd530c97451c6c6f
erik696969-a11y <erik.696969@gmail.com>
2026-08-15 23:09:45 +0200

    remove old v7 duplicate

--- app/community-forum-v7/app/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center text-xs text-harbor/50 py-6 px-4">
-      <p className="font-semibold">Hacienda del Señorío de Cifuentes</p>
-      <p>Calle Torre Campanillas 7, Benahavís 29679, España</p>
-    </footer>
-  );
-}
